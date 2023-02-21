fed700760e91351d055cba1dbc9bc42194f24957
keerthanasriniv <keerthanasrinivasan617@gmail.com>
2023-02-21 12:34:01 +0530

    First commit

---
+++ src/main/java/com/example/demo/KeerthsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeerthsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeerthsApplication.class, args);
+	}
+
+}
